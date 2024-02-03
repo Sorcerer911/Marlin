9e11d5b97b1e6a532d5e4ddc6af472d06b6a5b5f
Sorcerer911 <36299159+Sorcerer911@users.noreply.github.com>
2024-02-03 14:27:28 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1125,7 +1125,7 @@
 
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
-#define ENDSTOP_INTERRUPTS_FEATURE //CRE8
+//#define ENDSTOP_INTERRUPTS_FEATURE //CRE8
 
 /**
  * Endstop Noise Threshold
